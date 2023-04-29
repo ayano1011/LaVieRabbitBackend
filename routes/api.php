914ced17777d0804b903ac19fb1b0667832bf7c8
shouhei <shohei.nagamura@gmail.com>
2023-04-29 12:47:04 +0900

    環境構築完了

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\WeightController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix("weight")
+    ->name("weight.")
+    ->group(function () {
+        Route::get("", [WeightController::class, "index"])->name("index");
+        Route::post("", [WeightController::class, "store"])->name("store");
+    });
